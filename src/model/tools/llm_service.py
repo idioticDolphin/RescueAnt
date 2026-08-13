c38e20bcf699d443bbce7b0afe53f8045870c173
johannes <wichertj@students.uni-marburg.de>
2026-08-14 01:13:39 +0200

    context window change added

--- src/model/tools/llm_service.py
+++ src/model/tools/llm_service.py
@@ -4,18 +4,23 @@ _initialized_model_mapper = dict()
 _initialized_models = []
 
 def get_model_id(model_path, max_tokens):
-    global initialized_model_mapper
-    next_id = len(initialized_model_mapper)
+    global _initialized_model_mapper
+    next_id = len(_initialized_model_mapper)
     model_key = f"{model_path};{max_tokens}"
-    if model_key not in initialized_model_mapper.keys():
-        initialized_model_mapper[model_key] = next_id
+    if model_key not in _initialized_model_mapper.keys():
+        _initialized_model_mapper[model_key] = next_id
         init_model(model_path, max_tokens)
         return next_id
-    return initialized_model_mapper[model_key]
+    return _initialized_model_mapper[model_key]
 
 def get_model(id: int):
     return _initialized_models[id]
 
-def init_model(model_path, max_tokens):
+def init_model(model_path, context):
     global _initialized_models
-    _initialized_models.append(Llama(model_path)) # TODO: Figure out changing the context window
+    _initialized_models.append(
+        Llama(
+            model_path=model_path,
+            n_ctx = context
+        )
+    )
