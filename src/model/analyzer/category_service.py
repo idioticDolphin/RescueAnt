f6ce4893bf410ac7a6e6ad1dc42e116849927ef5
johannes <wichertj@students.uni-marburg.de>
2026-08-15 17:44:25 +0200

    Extraction works on minimal example

--- src/model/analyzer/category_service.py
+++ src/model/analyzer/category_service.py
@@ -3,15 +3,16 @@ import model.tools.llm_service as llm_service
 from llama_cpp import LlamaGrammar
 
 from model.analyzer import cleaning_service
+from model.objects.category import Category
 
 config = config_service.get_config()
 
 def categorize_website(html):
     llm_id = config.get_category_model_id()
     llm = llm_service.get_model(llm_id)
-    prompt = config.get_category_prompt()
     categories = config.get_categories()
     category_string = '"' + '" | "'.join([category.name for category in categories]) + '"'
+    prompt = f"{config.get_category_prompt()} The categories are {category_string}"
     grammar = LlamaGrammar.from_string(f'root ::= {category_string}')
     site_content = cleaning_service.clean(html, deduplicate=True)
     max_tokens = config.category_max_tokens
@@ -26,5 +27,5 @@ def categorize_website(html):
         max_tokens=max_tokens,
     )
     found_category = result['choices'][0]['message']['content']
-    print(f"Found category: {found_category}")
-    return found_category
+    print(f"Predicted category: {found_category}")
+    return config.get_category(found_category)
--- src/model/exceptions.py
+++ src/model/exceptions.py
@@ -1,2 +1,6 @@
 class ConfigError(Exception):
     pass
+
+class CategoryNotFoundError(Exception):
+    def __init__(self, category_name):
+        print(f"Category {category_name} not found")
